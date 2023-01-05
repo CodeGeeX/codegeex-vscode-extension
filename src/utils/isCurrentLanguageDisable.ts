98a283f50896e6e8a818c6bdd74292ffcde3f8ab
andiwang1993 <andi.wang93@hotmail.com>
2023-01-05 11:24:33 +0800

    Add files via upload

---
+++ src/utils/isCurrentLanguageDisable.ts
@@ -0,0 +1,17 @@
+import * as vscode from 'vscode';
+import { disabledFor } from '../param/configures';
+
+
+export const isCurrentLanguageDisable=() =>{
+    let editor = vscode.window.activeTextEditor;
+    if(!editor){
+        return false
+    }else{
+        const languageId = editor.document.languageId;
+        if((disabledFor as any)[languageId] === true || (disabledFor as any)[languageId] === 'true'){
+            return true
+        }else{
+            return false
+        }
+    }
+}
